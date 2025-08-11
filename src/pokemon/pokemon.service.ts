874ae645ffab64afa8fc354c661ee3d69ae798df
ryurzinn <chinwenwencha47@gmail.com>
2025-08-11 13:17:57 -0300

    Finalizada la config de las variables de entorno

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,14 +5,23 @@ import { Pokemon } from './entities/pokemon.entity';
 import { isValidObjectId, Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
 
+  private deafultLimit: number;
+
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
-  ) {}
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly configService: ConfigService,
+  ) {
+  
+    this.deafultLimit = configService.get<number>('defaultLimit') ?? 7;
+   
+  }
 
 
   async create(createPokemonDto: CreatePokemonDto) {
@@ -29,7 +38,7 @@ export class PokemonService {
 
   findAll(paginationDto: PaginationDto) {
 
-    const {limit = 10, offset = 0} = paginationDto;
+    const {limit = this.deafultLimit, offset = 0} = paginationDto;
 
     return this.pokemonModel.find()
     .limit(limit)
