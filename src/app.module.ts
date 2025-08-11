8c8d5d07190b0f111b0be6ba35c95d1e95d56838
ryurzinn <chinwenwencha47@gmail.com>
2025-08-11 16:14:50 -0300

    correcion para probar despliegue por tercera vez, cambios en el app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path'; // EN NODE
+import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EnvConfiguration } from './config/env.config';
 import { JoiValidationSchema } from './config/joi.validation';
 
@@ -17,17 +17,20 @@ import { JoiValidationSchema } from './config/joi.validation';
     }),
 
     ServeStaticModule.forRoot({ 
- rootPath: join(__dirname,'..','public'), 
- }),
+      rootPath: join(__dirname,'..','public'), 
+    }),
 
-    MongooseModule.forRoot( process.env.MONGODB ?? '', {
-      dbName: 'pokemonsdb'
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB', { infer: true }),
+        dbName: 'pokemonsdb'
+      }),
+      inject: [ConfigService],
     }),
 
     PokemonModule,
-
     CommonModule,
-
     SeedModule 
   ],
 })
